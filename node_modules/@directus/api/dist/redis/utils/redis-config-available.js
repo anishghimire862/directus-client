2c77f1c699af0e13b0ea4adfb4e3e23dda941238
Anish Ghimire <anishghimire862@gmail.com>
2024-02-14 11:47:59 +0545

    add gitignore

---
+++ node_modules/@directus/api/dist/redis/utils/redis-config-available.js
@@ -0,0 +1,8 @@
+import { useEnv } from '@directus/env';
+/**
+ * Check if Redis configuration exists in the current project's environment configuration
+ */
+export const redisConfigAvailable = () => {
+    const env = useEnv();
+    return 'REDIS' in env || Object.keys(env).some((key) => key.startsWith('REDIS_'));
+};
